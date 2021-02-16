5b10adbb54bdc9d3b132292a41d74f1aabf6849e
Uwe Trotzek <uwe.trotzek@gmail.com>
2021-02-16 15:03:02 +0100

    [TASK] Run linter only when necessary

--- .git-hooks/pre-commit-cs-fixer.sh
+++ .git-hooks/pre-commit-cs-fixer.sh
@@ -15,14 +15,20 @@ ESLINT="$GIT_DIR/../bin/yarn.sh lint"
 echo -e "${BLUE}executing cs fixer${NC}"
 #identify staged php files and execute cs linter
 PHP_FILES=$(git status --porcelain | egrep -i '^\s*[AM].*.(php)$' | cut -c 3- | cut -d'/' -f2- | tr '\n' ' ')
-$COMPOSER_PATH run-script csFixer $PHP_FILES
-for dir in $PHP_FILES; do
-    git add "app/$dir"
-done
+
+if [[ $PHP_FILES != "" ]]; then
+    $COMPOSER_PATH run-script csFixer $PHP_FILES
+    for dir in $PHP_FILES; do
+        git add "app/$dir"
+    done
+fi
 
 echo -e "${BLUE}executing eslint${NC}"
 #identify staged vue and js files and execute eslint (correct the path by suppressing /app/resources/js)
 JS_FILES=$(git status --porcelain | egrep -i '^\s*[AM].*.(vue|js)$' | cut -c 3- | tr '\n' ' ' | sed -e 's#app/resources/js/##g')
-$ESLINT $JS_FILES
+
+if [[ $JS_FILES != "" ]]; then
+    $ESLINT $JS_FILES
+fi
 
 echo -e "${GREEN}Linting passed. Committing files..${NC}"
